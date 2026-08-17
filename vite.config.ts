7000b061f037c5e261408552641e35d85e251e86
xhp-gif <2286680554@qq.com>
2026-08-17 17:38:46 +0800

    fix(vite): 忽略 release 和 dist 目录避免打包时热重载监听锁死崩溃

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ export default defineConfig({
   server: {
     host: '127.0.0.1',
     port: 9527,
+    watch: {
+      ignored: ['**/release/**', '**/dist/**'],
+    },
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:4174',
